b374c27e332dfae5656deec8011672badc001b0f
sofmorais <sofiassmorais@gmail.com>
2023-11-24 11:49:34 -0300

    updating

--- src/exerciciosEstruturaCondicional/exercicio07.java
+++ src/exerciciosEstruturaCondicional/exercicio07.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class exercicio07 {
     public static void main(String[] args) {
         /* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas*/
+
         Scanner sc = new Scanner(System.in);
 
         int horaInicio, horaFinal, duracaoJogo;
